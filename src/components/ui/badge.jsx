7c0bc3d6fe1f7b3741d594f119064de9338e4a84
Xavier K. <59243836+xk2800@users.noreply.github.com>
2024-02-21 00:23:10 +0800

    updates la

---
+++ src/components/ui/badge.jsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { cva } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const badgeVariants = cva(
+  "inline-flex items-center rounded-[50px] mt-xxxxs border px-[10px] pt-[4px] pb-[5px] text-[14px] transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  {
+    variants: {
+      variant: {
+        default: "border-transparent bg-primary text-primary-foreground",
+        secondary:
+          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        destructive:
+          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
+        outline: "text-foreground",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
+
+function Badge({ className, variant, ...props }) {
+  return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+  );
+}
+
+export { Badge, badgeVariants };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,24 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: ["class"],
-  content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './app/**/*.{js,jsx}',
-    './src/**/*.{js,jsx}',
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   prefix: "",
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: {
+        DEFAULT: '1.5rem',
+        sm: '2rem',
+      },
       screens: {
         "2xl": "1400px",
       },
     },
+    fontSize: {
+      xs: '1rem', // 18px mobile
+      sm: '1.125rem', // 18px desktop // 20px mobile
+      base: '1.25rem', // 20px desktop // 22px mobile
+      lg: '1.375rem', // 22px desktop // 36px mobile
+      xl: '2.25rem', // 36px desktop // 48px mobile
+      '2xl': '3rem', // 48px desktop
+    },
     extend: {
       colors: {
+        backgroundColor: '#F3F4F6',
+        baseTextColor: '#181818',
+        baseTextCTAColor: '#0F0F0F',
+        subTextColor: '#4E4E4E',
+        subTextColorHover: '#6B7280',
+        primaryCTAColor: '#9B111A',
+        primaryCTAColorHover: '#740C13',
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
@@ -72,6 +84,34 @@ export default {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        lexendDeca: ['Lexend Deca', 'sans-serif'],
+      },
+      padding: {
+        'xxxxs': '8px',
+        'xxxs': '12px',
+        'xxs': '16px',
+        'xs': '24px',
+        's': '36px',
+        'm': '48px',
+        'l': '56px',
+        'xl': '72px',
+        'xxl': '96px',
+        'xxxl': '120px',
+      },
+      margin: {
+        'xxxxs': '8px',
+        'xxxs': '12px',
+        'xxs': '16px',
+        'xs': '24px',
+        's': '36px',
+        'm': '48px',
+        'l': '56px',
+        'xl': '72px',
+        'xxl': '96px',
+        'xxxl': '120px',
+      }
+
     },
   },
   plugins: [require("tailwindcss-animate")],
